0937a6c1908e342f8f429de755bc1c717e43b88b
sadokacacha <74996499+sadokacacha@users.noreply.github.com>
2021-01-07 22:55:34 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -7,18 +7,23 @@ for (var i =0; i < remove_cart_item_button.length; i++){
     var button = remove_cart_item_button[i]
     button.addEventListener ('click', function(event) {
         var buttonclicked = event.target  
-        buttonclicked.parentElement.parentElement.remove()
+        buttonclicked.parentElement.parentElement.parentElement.remove()
         update_cart_total()
     })
 }
-
-
 //functions
-function update_cart_total(){
-var cart_item_container =document.getElementsByClassName('cake_cart')[0]
-var cart_rows =document.getElementsByClassName('cart_cake_to_buy')
-for (var i =0; i < cart_rows.length; i++){
-    var cart_rows =cart_rows[i]
-    var price_element =cart_rows.getElementsByClassName('cart_price')[0]
+function update_cart_total() {
+    var cart_item_container = document.getElementsByClassName('cake_cart')[0]
+    var cart_rows= cart_item_container.getElementsByClassName('cart_rows')
+    var total = 0
+    for (var i =0; i < cart_rows.length; i++){
+        var cart_rows=cart_rows[i]
+        var price_element =cart_rows.getElementsByClassName('price')[0]
+        var quantity_element=cart_rows.getElementsByClassName('quantity') [0]
+        var price = parseFloat( price_element.innerText)
+        var quantity =quantity_element.value
+        total=total+(price*quantity)
+        
 }
+document.getElementsByClassName('total_price')[0].innerText = total
 }
